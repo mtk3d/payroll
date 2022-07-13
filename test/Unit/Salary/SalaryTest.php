7887772eca81b709faec4924b8cd78003e0e49bf
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-07-13 18:49:54 +0200

    test permanent bonus

--- test/Unit/Salary/SalaryTest.php
+++ test/Unit/Salary/SalaryTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Test\Unit\Salary;
 
+use Carbon\Carbon;
 use Money\Money;
 use Payroll\Salary\Domain\BonusRule\PercentageBonus;
+use Payroll\Salary\Domain\BonusRule\PermanentBonus;
 use Payroll\Salary\Domain\Department;
 use Payroll\Salary\Domain\Employee;
 use Payroll\Shared\DepartmentId;
@@ -18,11 +20,25 @@ class SalaryTest extends TestCase
     {
         $employeeId = EmployeeId::newOne();
         $departmentId = DepartmentId::newOne();
+        $employmentDate = Carbon::now()->subYears(5);
         $baseSalary = Money::USD(110000);
         $bonus = new PercentageBonus(10);
         $department = new Department($departmentId, $bonus);
-        $employee = new Employee($employeeId, $baseSalary, $department);
+        $employee = new Employee($employeeId, $employmentDate, $baseSalary, $department);
 
-        self::assertEquals(Money::USD(121000), $employee->fullSalary());
+        self::assertEquals(Money::USD(121000), $employee->fullSalary);
+    }
+
+    public function testPermanentBonus(): void
+    {
+        $employeeId = EmployeeId::newOne();
+        $departmentId = DepartmentId::newOne();
+        $employmentDate = Carbon::now()->subYears(10);
+        $baseSalary = Money::USD(100000);
+        $bonus = new PermanentBonus(Money::USD(10000));
+        $department = new Department($departmentId, $bonus);
+        $employee = new Employee($employeeId, $employmentDate, $baseSalary, $department);
+
+        self::assertEquals(Money::USD(200000), $employee->fullSalary);
     }
 }
