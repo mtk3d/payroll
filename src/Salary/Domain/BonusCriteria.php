aaeba3c2a2487a9c816a744e9eeba44549a6de00
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-07-13 18:50:17 +0200

    create bonus criteria to calculate

--- src/Salary/Domain/BonusCriteria.php
+++ src/Salary/Domain/BonusCriteria.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Payroll\Salary\Domain;
 
+use Carbon\Carbon;
+use Money\Money;
+
 class BonusCriteria
 {
-
+    public function __construct(readonly Carbon $employmentDate, readonly Money $baseSalary) {}
 }
