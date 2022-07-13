db74c8573d3b653467544288ad3c82e340a26a1f
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-07-13 18:24:33 +0200

    prepare common shared objects

---
+++ src/Shared/AbstractId.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Payroll\Shared;
+
+use Stringable;
+
+abstract class AbstractId implements Stringable
+{
+    public function __construct(protected UUID $id)
+    {
+    }
+
+    public function id(): UUID
+    {
+        return $this->id;
+    }
+
+    public function toString(): string
+    {
+        return $this->id->toString();
+    }
+
+    public function __toString()
+    {
+        return $this->toString();
+    }
+}
---
+++ src/Shared/UUID.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Payroll\Shared;
+
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+class UUID
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        if (!RamseyUuid::isValid($value)) {
+            throw new \InvalidArgumentException('Invalid UUID format');
+        }
+
+        $this->value = $value;
+    }
+
+    public static function random(): self
+    {
+        return new self(RamseyUuid::getFactory()->uuid4()->toString());
+    }
+
+    public function toString(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public function isEqual(self $id): bool
+    {
+        return $this->value === $id->value;
+    }
+}
