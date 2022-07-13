9a2f61f93ef63892254715d805d20bd576dfc3be
Mateusz Cholewka <mateusz@cholewka.com.pl>
2022-07-13 20:58:51 +0200

    refactor test

---
+++ test/Fixture/EmployeeFixture.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use Money\Money;
+use Payroll\Salary\Domain\Bonus\BonusType;
+use Payroll\Salary\Domain\BonusRule;
+use Payroll\Salary\Domain\Department;
+use Payroll\Salary\Domain\Employee;
+use Payroll\Shared\DepartmentId;
+use Payroll\Shared\EmployeeId;
+
+function aEmployee(?DateTimeImmutable $employmentDate = null, ?Money $baseSalary = null, ?Department $department = null): Employee
+{
+    if (is_null($employmentDate)) {
+        $employmentDate = new DateTimeImmutable("2005-03-14");
+    }
+
+    if (is_null($baseSalary)) {
+        $baseSalary = Money::USD(110000);
+    }
+
+    if (is_null($department)) {
+        $department = aDepartment();
+    }
+
+    $employeeId = EmployeeId::newOne();
+
+    return new Employee($employeeId, $employmentDate, $baseSalary, $department);
+}
+
+function aDepartment(?BonusType $bonusType = null, ?int $value = null): Department
+{
+    if (is_null($bonusType)) {
+        $bonusType = BonusType::PERCENTAGE;
+    }
+
+    if (is_null($value)) {
+        $value = 1000;
+    }
+
+    $departmentId = DepartmentId::newOne();
+    return new Department($departmentId, new BonusRule($bonusType, $value));
+}
--- test/Unit/Salary/SalaryTest.php
+++ test/Unit/Salary/SalaryTest.php
@@ -10,16 +10,9 @@ use Payroll\Salary\Application\CalculateSalariesHandler;
 use Payroll\Salary\Application\Command\CalculateSalaries;
 use Payroll\Salary\Domain\Bonus\BonusCalculatorFactory;
 use Payroll\Salary\Domain\Bonus\BonusType;
-use Payroll\Salary\Domain\Bonus\PercentageBonus;
-use Payroll\Salary\Domain\Bonus\PermanentBonus;
-use Payroll\Salary\Domain\BonusRule;
-use Payroll\Salary\Domain\Department;
-use Payroll\Salary\Domain\Employee;
 use Payroll\Salary\Domain\SalaryCalculated;
 use Payroll\Salary\Infrastructure\Repository\InMemoryEmployeeRepository;
 use Payroll\Shared\Clock;
-use Payroll\Shared\DepartmentId;
-use Payroll\Shared\EmployeeId;
 use Payroll\Shared\InMemoryDomainEventBus;
 use PHPUnit\Framework\TestCase;
 
@@ -33,50 +26,44 @@ class SalaryTest extends TestCase
         $this->now = new DateTimeImmutable("2005-03-14");
         $this->clock = self::createMock(Clock::class);
         $this->clock->method('now')->willReturn($this->now);
+        $this->calculatorFactory = new BonusCalculatorFactory($this->clock);
+        $this->bus = new InMemoryDomainEventBus();
     }
 
     public function testPercentageBonus(): void
     {
-        $employeeId = EmployeeId::newOne();
-        $departmentId = DepartmentId::newOne();
         $employmentDate = $this->now->modify("-5 years");
         $baseSalary = Money::USD(110000);
-        $bonus = new PercentageBonus(1000);
-        $department = new Department($departmentId, new BonusRule(BonusType::PERCENTAGE, 1000));
-        $employee = new Employee($employeeId, $employmentDate, $baseSalary, $department);
+        $department = aDepartment(BonusType::PERCENTAGE, 1000);
+        $employee = aEmployee($employmentDate, $baseSalary, $department);
+        $calculator = $this->calculatorFactory->create($department->bonusRule);
 
-        self::assertEquals(Money::USD(121000), $bonus->calculate($employee->bonusCriteria()));
+        self::assertEquals(Money::USD(121000), $calculator->calculate($employee->bonusCriteria()));
     }
 
     public function testPermanentBonus(): void
     {
-        $employeeId = EmployeeId::newOne();
-        $departmentId = DepartmentId::newOne();
         $employmentDate = $this->now->modify("-10 years");
         $baseSalary = Money::USD(100000);
+        $department = aDepartment(BonusType::PERMANENT, 10000);
+        $employee = aEmployee($employmentDate, $baseSalary, $department);
+        $calculator = $this->calculatorFactory->create($department->bonusRule);
 
-        $bonus = new PermanentBonus($this->clock, 10000);
-        $department = new Department($departmentId, new BonusRule(BonusType::PERMANENT, 10000));
-        $employee = new Employee($employeeId, $employmentDate, $baseSalary, $department);
-
-        self::assertEquals(Money::USD(200000), $bonus->calculate($employee->bonusCriteria()));
+        self::assertEquals(Money::USD(200000), $calculator->calculate($employee->bonusCriteria()));
     }
 
     public function testCalculateSalaries(): void
     {
-        $employeeId = EmployeeId::newOne();
-        $departmentId = DepartmentId::newOne();
         $employmentDate = $this->now->modify("-10 years");
         $baseSalary = Money::USD(100000);
-        $department = new Department($departmentId, new BonusRule(BonusType::PERMANENT, 10000));
-        $employee = new Employee($employeeId, $employmentDate, $baseSalary, $department);
+        $department = aDepartment(BonusType::PERMANENT, 10000);
+        $employee = aEmployee($employmentDate, $baseSalary, $department);
 
-        $bus = new InMemoryDomainEventBus();
         $repository = new InMemoryEmployeeRepository([$employee]);
-        $calculatorFactory = new BonusCalculatorFactory($this->clock);
-        $handler = new CalculateSalariesHandler($bus, $repository, $calculatorFactory);
+
+        $handler = new CalculateSalariesHandler($this->bus, $repository, $this->calculatorFactory);
         $handler->handle(new CalculateSalaries());
 
-        self::assertEquals(new SalaryCalculated($employeeId, Money::USD(200000)), $bus->latestEvent());
+        self::assertEquals(new SalaryCalculated($employee->employeeId, Money::USD(200000)), $this->bus->latestEvent());
     }
 }
